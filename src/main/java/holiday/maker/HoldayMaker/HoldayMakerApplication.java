037a9258bf472a17e5c52d270af41ed0f9be8b51
ollejj <ollejohansson1996@gmail.com>
2020-09-09 10:19:17 +0200

    Empty Spring proj

---
+++ src/main/java/holiday/maker/HoldayMaker/HoldayMakerApplication.java
@@ -0,0 +1,13 @@
+package holiday.maker.HoldayMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldayMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldayMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/holiday/maker/HoldayMaker/HoldayMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package holiday.maker.HoldayMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldayMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
