92b3df8a1432746c240c91884c995c3d7cbbc890
Frida Ek <frida.ek.1988@gmail.com>
2020-09-10 15:01:17 +0200

    skapat customer service

--- src/main/java/com/group/foctg/holidayMaker/services/CustomerService.java
+++ src/main/java/com/group/foctg/holidayMaker/services/CustomerService.java
@@ -1,5 +1,40 @@
 package com.group.foctg.holidayMaker.services;
 
-public class CustomerService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.group.foctg.holidayMaker.model.Accommodation;
+import com.group.foctg.holidayMaker.model.Booking;
+import com.group.foctg.holidayMaker.model.Customer;
+import com.group.foctg.holidayMaker.repositories.CustomerRepo;
+
+public class CustomerService {
+	
+	@Autowired
+	private CustomerRepo customerRepo;
+	
+	public boolean saveCustomer(Customer customer) {
+		return true;
+	}
+	
+	public boolean removeCustomer(Customer customer) {
+		return true;
+	}
+	
+	public Customer findOne(Long ID) {
+		return null;
+	}
+	
+	public Customer findCustomerByEmail(String email) {
+		return null;
+	}
+	
+	public List<Booking> findCustomersBookingsByCustomerID(Long ID){
+		return null;
+	}
+	
+	public List<Accommodation> findCustomersAccommodationsByCustomerID(Long ID){
+		return null;
+	}
 }
