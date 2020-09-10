4af2ed18db4d1b82bb5d34b663748cc64b2880aa
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-09-10 17:28:46 +0200

    Added Queries for all endpoints

--- src/main/java/com/group/foctg/holidayMaker/repositories/CustomerRepo.java
+++ src/main/java/com/group/foctg/holidayMaker/repositories/CustomerRepo.java
@@ -8,13 +8,17 @@ import org.springframework.stereotype.Repository;
 import com.group.foctg.holidayMaker.model.Accommodation;
 import com.group.foctg.holidayMaker.model.Booking;
 import com.group.foctg.holidayMaker.model.Customer;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
-
-	Customer findCustomerByEmail(String email);
-	
-	List<Booking> findBookingsByID(Long ID);
-	
-	List<Accommodation> findAccommodationsByID(Long ID);
+    
+    @Query("SELECT c FROM Customer c WHERE c.email = ?1")
+    Customer findCustomerByEmail(String email);
+    
+    @Query("SELECT c.bookings FROM Customer c WHERE c.id = ?1")
+    List<Booking> findBookingsByID(Long ID);
+    
+    @Query("SELECT c.accommodations FROM Customer c WHERE c.id = ?1")
+    List<Accommodation> findAccommodationsByID(Long ID);
 }
