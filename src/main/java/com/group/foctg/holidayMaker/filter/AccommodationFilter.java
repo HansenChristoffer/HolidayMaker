5433df22f4d5a819fbbd1ed9974027003f22702b
ollejj <ollejohansson1996@gmail.com>
2020-09-10 14:12:13 +0200

    wrote Filter.java to store filter values, and AccommodationFilter.java to handle the filter process

---
+++ src/main/java/com/group/foctg/holidayMaker/filter/AccommodationFilter.java
@@ -0,0 +1,17 @@
+package com.group.foctg.holidayMaker.filter;
+
+import java.util.List;
+
+import com.group.foctg.holidayMaker.model.Accommodation;
+import com.group.foctg.holidayMaker.model.Filter;
+
+public class AccommodationFilter {
+		
+	public static List<Accommodation> getFilteredAccommodations(Filter filter, List<Accommodation> accommodations) {
+		
+		//do stuff
+		
+		return null;
+	}
+	
+}
