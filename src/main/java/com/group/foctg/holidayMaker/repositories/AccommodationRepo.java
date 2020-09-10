8e1192e9e5119cd8f17afa7558a79fcec0e0e001
ollejj <ollejohansson1996@gmail.com>
2020-09-10 12:10:09 +0200

    Wrote accommodationService and added @Repository infront of AccommodationRepo

--- src/main/java/com/group/foctg/holidayMaker/repositories/AccommodationRepo.java
+++ src/main/java/com/group/foctg/holidayMaker/repositories/AccommodationRepo.java
@@ -3,14 +3,15 @@ package com.group.foctg.holidayMaker.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.group.foctg.holidayMaker.model.Accommodation;
 import com.group.foctg.holidayMaker.model.Customer;
 import com.group.foctg.holidayMaker.filter.Filter;
 
+@Repository
 public interface AccommodationRepo extends JpaRepository<Accommodation, Long> {
 
-	List<Accommodation> findAccommodationsByUser(Customer customer);
-	List<Accommodation> findAccommodationsByFilter(Filter filter);
+	List<Accommodation> findAccommodationsByCustomerID(Long customer_id);
 	
 }
---
+++ src/main/java/com/group/foctg/holidayMaker/services/AccommodationService.java
@@ -0,0 +1,52 @@
+package com.group.foctg.holidayMaker.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.group.foctg.holidayMaker.model.Accommodation;
+import com.group.foctg.holidayMaker.model.Customer;
+import com.group.foctg.holidayMaker.repositories.AccommodationRepo;
+
+public class AccommodationService {
+	
+	@Autowired
+	private AccommodationRepo accommodationRepository;
+	
+	public boolean saveAccommodation(Accommodation accommodation) {
+		return false;
+	}
+	
+	public boolean removeAccommodation(Accommodation accommodation) {
+		return false;
+	}
+	
+	//Sounds good, doesnt work
+	public boolean updateAccommodation(Accommodation accommodation) {
+		return false;
+	}
+	
+	//mitt förslag
+	//Accommodation Entity SetID required
+	//public boolean updateAccommodation(Accommodation old, Accommodation new) {
+	//  new.setID(old);
+	//	old = new
+	//	
+	//  return false;
+	//}
+	
+	public List<Accommodation> findAll() {
+		return null;
+	}
+	
+	public Accommodation findOne(Long id) {
+		return null;
+	}
+	
+	public List<Accommodation> findAccommodationsByUser(Customer customer) {
+		return null;
+	}
+	
+	
+	
+}
