2d1e760743550890ce52d79eecfbd5d30a368e68
Frida Ek <frida.ek.1988@gmail.com>
2020-09-10 14:13:14 +0200

    skapat customer repo

---
+++ src/main/java/com/group/foctg/holidayMaker/repositories/CustomerRepo.java
@@ -0,0 +1,20 @@
+package com.group.foctg.holidayMaker.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.group.foctg.holidayMaker.model.Accommodation;
+import com.group.foctg.holidayMaker.model.Booking;
+import com.group.foctg.holidayMaker.model.Customer;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer, Long>{
+
+	Customer findCustomerByEmail(String email);
+	
+	List<Booking> findCustomersBookingsByCustomerID(Long ID);
+	
+	List<Accommodation> findCustomersAccommodationsByCustomerID(Long ID);
+}
