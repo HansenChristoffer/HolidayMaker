ef78578d2086a93b6839f78c8658c73b239802e0
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-09-11 11:13:45 +0200

    Customer copyright & documentation
    Added a Apache Licence copyright ontop of the class
    Added Javadocs for the class itself

--- src/main/java/com/group/foctg/holidayMaker/model/Customer.java
+++ src/main/java/com/group/foctg/holidayMaker/model/Customer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020-2030 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.group.foctg.holidayMaker.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -11,6 +27,15 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
+/**
+ * The Customer entity class. Holds the various fields that are required 
+ * for the functionality of the program.
+ * These fields are also turned into columns in the SQLite3 database.
+ * There is also two OneToMany relationships with other entity classes.
+ * 
+ * @author Olle Johansson
+ * @author Christoffer Hansen
+ */
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) 
 public class Customer implements Serializable {
