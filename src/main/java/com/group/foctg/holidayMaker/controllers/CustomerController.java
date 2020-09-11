77e9914bcdf88a043785baffbc597b7de87d6626
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-09-11 13:03:10 +0200

    CustomerController documentation
    Added Apache licence copyright ontop of class
    Added Javadoc for class
    Added Javadocs for all the methods

--- src/main/java/com/group/foctg/holidayMaker/controllers/CustomerController.java
+++ src/main/java/com/group/foctg/holidayMaker/controllers/CustomerController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020-2030 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.group.foctg.holidayMaker.controllers;
 
 import com.group.foctg.holidayMaker.model.Accommodation;
@@ -6,7 +21,6 @@ import com.group.foctg.holidayMaker.model.Customer;
 import com.group.foctg.holidayMaker.services.CustomerService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,46 +28,105 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * RestController for the <code>Customer</code> entity and column. This class
+ * holds all the endpoints for <code>Customer</code>.
+ *
+ * Autowiring {@link com.group.foctg.holidayMaker.services.CustomerService}.
+ *
+ * The default URL value is set to, "/api".
  *
  * @author Christoffer Hansen <christoffer.hansen.hc@gmail.com>
+ * @see com.group.foctg.holidayMaker.services.CustomerService
  */
-
 @RestController
 @RequestMapping(value = "/api")
 public class CustomerController {
-    
+
     @Autowired
     private CustomerService customerService;
-    
+
+    /**
+     * POST endpoint method that listens on <code>"/customer"</code> URL and
+     * will call the
+     * {@link com.group.foctg.holidayMaker.services.CustomerService#saveCustomer(com.group.foctg.holidayMaker.model.Customer)}
+     * method from the Service.
+     *
+     * @param customer <code>Customer</code> object to pass to the Service class
+     * @return a boolean value from the autowired Service
+     */
     @RequestMapping(value = "/customer", method = RequestMethod.POST)
     public boolean saveCustomer(@RequestBody Customer customer) {
         return customerService.saveCustomer(customer);
     }
-    
+
+    /**
+     * DELETE endpoint method that listens on <code>"/customer"</code> URL and
+     * will call the
+     * {@link com.group.foctg.holidayMaker.services.CustomerService#removeCustomer(java.lang.Long)}
+     * method from the autowired Service.
+     *
+     * @param id Long value to pass to the Service class
+     * @return a boolean value from the Service
+     */
     @RequestMapping(value = "/customer", method = RequestMethod.DELETE)
     public boolean removeCustomer(@RequestParam Long id) {
         return customerService.removeCustomer(id);
     }
-    
+
+    /**
+     * GET endpoint method that listens on <code>"/customer"</code> URL and will
+     * call the
+     * {@link com.group.foctg.holidayMaker.services.CustomerService#getOne(java.lang.Long)}
+     * method from the autowired Service.
+     *
+     * @param id Long value to pass to the Service class
+     * @return a boolean value from the Service
+     */
     @RequestMapping(value = "/customer", method = RequestMethod.GET)
     public Customer findCustomerById(@RequestParam Long id) {
         return customerService.getOne(id);
     }
-    
+
+    /**
+     * GET endpoint method that listens on <code>"/customer/by"</code> URL and
+     * will call the
+     * {@link com.group.foctg.holidayMaker.services.CustomerService#findCustomerByEmail(java.lang.String)}
+     * method from the autowired Service.
+     *
+     * @param email String value to pass to the Service class
+     * @return a boolean value from the Service
+     */
     @RequestMapping(value = "/customer/by", method = RequestMethod.GET)
     public Customer findCustomerByEmail(@RequestParam String email) {
         return customerService.findCustomerByEmail(email);
     }
-    
+
+    /**
+     * GET endpoint method that listens on <code>"/customer/bookings"</code> URL
+     * and will call the
+     * {@link com.group.foctg.holidayMaker.services.CustomerService#findCustomersBookingsByCustomerID(java.lang.Long)}
+     * method from the autowired Service.
+     *
+     * @param id Long value to pass to the Service class
+     * @return a boolean value from the Service
+     */
     @RequestMapping(value = "/customer/bookings", method = RequestMethod.GET)
     public List<Booking> findBookingsByCustomerId(@RequestParam Long id) {
         return customerService.findCustomersBookingsByCustomerID(id);
     }
-    
+
+    /**
+     * GET endpoint method that listens on
+     * <code>"/customer/accommodations"</code> URL and will call the
+     * {@link com.group.foctg.holidayMaker.services.CustomerService#findCustomersAccommodationsByCustomerID(java.lang.Long)}
+     * method from the autowired Service.
+     *
+     * @param id Long value to pass to the Service class
+     * @return a boolean value from the Service
+     */
     @RequestMapping(value = "/customer/accommodations", method = RequestMethod.GET)
     public List<Accommodation> findAccommodationsByCustomerId(@RequestParam Long id) {
         return customerService.findCustomersAccommodationsByCustomerID(id);
     }
-    
-    
+
 }
