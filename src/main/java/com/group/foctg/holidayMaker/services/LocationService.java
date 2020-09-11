af33800f687f3acf3b535e7b9b705280b83a2d93
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-09-11 14:57:12 +0200

    LocationService documentation
    Added Apache license copyright ontop of class
    Added Javadocs for the class
    Added Javadocs for all the methods
    
    Removed the if in saveLocation() method as it was redundant
    
    Changed the .save function in saveLocation() to .saveAndFlush()

--- src/main/java/com/group/foctg/holidayMaker/services/LocationService.java
+++ src/main/java/com/group/foctg/holidayMaker/services/LocationService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020-2030 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.group.foctg.holidayMaker.services;
 
 import java.util.List;
@@ -9,38 +24,86 @@ import com.group.foctg.holidayMaker.model.Accommodation;
 import com.group.foctg.holidayMaker.model.Location;
 import com.group.foctg.holidayMaker.repositories.LocationRepository;
 
+/**
+ * Service class for the {@link com.group.foctg.holidayMaker.model.Location}
+ * column and entity. Autowires the repository.
+ *
+ * @author Olle Johansson
+ *
+ * @see com.group.foctg.holidayMaker.repositories.LocationRepository
+ */
 @Service
 public class LocationService {
 
-	@Autowired
-	private LocationRepository locationRepository;
+    @Autowired
+    private LocationRepository locationRepository;
 
-	public boolean saveLocation(Location location) {
-		if (locationRepository.save(location).equals(location)) {
-			return true;
-		} else
-			return false;
-	}
+    /**
+     * Saves the {@link com.group.foctg.holidayMaker.model.Location} object from
+     * parameter in the database.
+     *
+     * @param location {@link com.group.foctg.holidayMaker.model.Location}
+     * object that shall be saved
+     * @return A boolean value representing whether the saving was successful or
+     * not
+     */
+    public boolean saveLocation(Location location) {
+        return locationRepository.saveAndFlush(location).equals(location);
+    }
 
-	public boolean removeLocationByID(Long ID) {
-		if (locationRepository.existsById(ID)) {
-			Location found = locationRepository.getOne(ID);
-			locationRepository.delete(found);
-			return true;
-		} else
-			return false;
-	}
+    /**
+     * Removes the {@link com.group.foctg.holidayMaker.model.Location} object
+     * with the same <code>id</code> as the parameter from the database.
+     *
+     * @param id Long value used for finding and removing
+     * {@link com.group.foctg.holidayMaker.model.Location} with that
+     * <code>id</code>
+     * @return A boolean value representing whether the removing was successful
+     * or not
+     */
+    public boolean removeLocationByID(Long id) {
+        if (locationRepository.existsById(id)) {
+            Location found = locationRepository.getOne(id);
+            locationRepository.delete(found);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public List<Location> findAll() {
-		return locationRepository.findAll();
-	}
+    public List<Location> findAll() {
+        return locationRepository.findAll();
+    }
 
-	public Location getOne(Long ID) {
-		return locationRepository.getOne(ID);
-	}
+        /**
+     * Goes through the database, checks and returns a
+     * {@link com.group.foctg.holidayMaker.model.Location} object if a location
+     * with given <code>id</code> exists.
+     *
+     * @param id Long value to use for finding the
+     * {@link com.group.foctg.holidayMaker.model.Location}
+     * @return {@link com.group.foctg.holidayMaker.model.Location} object with
+     * the given <code>id</code>, if it exists
+     */
+    public Location getOne(Long id) {
+        return locationRepository.getOne(id);
+    }
 
-	public List<Accommodation> findAccommodationsByLocation(Long ID) {
-		return locationRepository.findAccommodationsByLocationID(ID);
-	}
+    /**
+     * Goes through the database, checks and returns all
+     * {@link com.group.foctg.holidayMaker.model.Accommodation} objects in the
+     * List&lt;{@link com.group.foctg.holidayMaker.model.Accommodation}&gt; if a
+     * location with given <code>id</code> exists.
+     *
+     * @param id Long value to use for finding the
+     * {@link com.group.foctg.holidayMaker.model.Location}
+     * @return
+     * List&lt;{@link com.group.foctg.holidayMaker.model.Accommodation}&gt; from
+     * the {@link com.group.foctg.holidayMaker.model.Location} with the given
+     * <code>id</code>, if it exists
+     */
+    public List<Accommodation> findAccommodationsByLocation(Long id) {
+        return locationRepository.findAccommodationsByLocationID(id);
+    }
 
 }
