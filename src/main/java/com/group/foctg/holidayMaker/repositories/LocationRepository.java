95b189a16c90b7600e0643815f98e4b7cf93bc70
ollejj <ollejohansson1996@gmail.com>
2020-09-11 10:30:23 +0200

    Done with LocationRepository.java and set a query that corresponds with function

---
+++ src/main/java/com/group/foctg/holidayMaker/repositories/LocationRepository.java
@@ -0,0 +1,17 @@
+package com.group.foctg.holidayMaker.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.group.foctg.holidayMaker.model.Customer;
+import com.group.foctg.holidayMaker.model.Location;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    
+    @Query("SELECT a FROM Accommodation a WHERE a.location_id = ?1")
+    Customer findAccommodationsByLocationID(Integer ID);
+}
