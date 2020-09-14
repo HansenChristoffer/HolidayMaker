87b50061e071e12c3d4b70ec14ff1bc1fa8253e5
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-09-14 10:13:26 +0200

    Added copyright and javadoc ontop for the class

--- src/main/java/com/group/foctg/holidayMaker/model/Booking.java
+++ src/main/java/com/group/foctg/holidayMaker/model/Booking.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020-2030 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.group.foctg.holidayMaker.model;
 
 import java.io.Serializable;
@@ -13,6 +28,17 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
 
+/**
+ * The {@link com.group.foctg.holidayMaker.model.Booking} entity class. Holds
+ * the various fields that are required for the functionality of the program.
+ * These fields are also turned into columns in the SQLite3 database. There is
+ * also one OneToMany and one ManyToOne relationships with other entity classes.
+ *
+ * Uses the SEQUENCE type for auto gen id values because of the restrictions
+ * that SQLite3 holds.
+ *
+ * @author Olle Johansson
+ */
 @Entity
 public class Booking implements Serializable {
 
