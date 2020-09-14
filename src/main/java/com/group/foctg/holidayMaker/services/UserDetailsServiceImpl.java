d787d09916ff4784e9ff8e4706e76c3b193a0e2a
ollejj <ollejohansson1996@gmail.com>
2020-09-14 23:12:42 +0200

    removed commented-out temp user-setting thing. its been a long fkn day, peace!

--- src/main/java/com/group/foctg/holidayMaker/services/UserDetailsServiceImpl.java
+++ src/main/java/com/group/foctg/holidayMaker/services/UserDetailsServiceImpl.java
@@ -19,40 +19,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
 	private CustomerRepository customerRepository;
-	
-	@Autowired
-	private CustomerService customerService;
 
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		
-		/*Customer test1 = new Customer();
-		
-		test1.setEmail("Olle");
-		test1.setPassword("123");
-		customerService.saveCustomer(test1);
-		
-		Customer test2 = new Customer();
-		
-		test2.setEmail("Frida");
-		test2.setPassword("123");
-		customerService.saveCustomer(test2);
-
-		Customer test3 = new Customer();
-		
-		test3.setEmail("Stoffe");
-		test3.setPassword("123");
-		customerService.saveCustomer(test3);
-
-		Customer test4 = new Customer();
-		
-		test4.setEmail("Tanvir");
-		test4.setPassword("123");
-		customerService.saveCustomer(test4);*/
-		
+				
 		Customer customer = customerRepository.findCustomerByEmail(email);
 
 		return User.builder().username(customer.getEmail()).password(bCryptPasswordEncoder.encode(customer.getPassword())).roles("USER").build();
