0ae4000e54f5c19efb4a90c321b303161d4b0ad5
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-09-14 10:00:31 +0200

    Added Javadocs for all methods Changed ID to id

--- src/main/java/com/group/foctg/holidayMaker/services/BookingService.java
+++ src/main/java/com/group/foctg/holidayMaker/services/BookingService.java
@@ -23,31 +23,79 @@ import org.springframework.stereotype.Service;
 import com.group.foctg.holidayMaker.model.Booking;
 import com.group.foctg.holidayMaker.repositories.BookingRepository;
 
+/**
+ * Service class for the {@link com.group.foctg.holidayMaker.model.Booking}
+ * column and entity. Autowires the repository.
+ *
+ * @author Frida Ek
+ *
+ * @see com.group.foctg.holidayMaker.repositories.BookingRepository
+ */
 @Service
 public class BookingService {
-	
-	@Autowired
-	private BookingRepository bookingRepository;
-	
-	
-	public boolean saveBooking(Booking booking) {
-		return bookingRepository.saveAndFlush(booking).equals(booking);
-	}
-	
-	public boolean removeBooking(Long ID) {
-		if (bookingRepository.existsById(ID)) {
-			bookingRepository.deleteById(ID);
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public boolean updateBooking(Booking booking) {
-		return bookingRepository.saveAndFlush(booking).equals(booking);
-	}
-	
-	public List<Booking> findBookingsByCustomerId(Long ID) {
-		return bookingRepository.findBookingsByCustomerID(ID);
-	}
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    /**
+     * Saves the {@link com.group.foctg.holidayMaker.model.Booking} object from
+     * parameter in the database.
+     *
+     * @param booking {@link com.group.foctg.holidayMaker.model.Booking} object
+     * that shall be saved
+     * @return A boolean value representing whether the saving was successful or
+     * not
+     */
+    public boolean saveBooking(Booking booking) {
+        return bookingRepository.saveAndFlush(booking).equals(booking);
+    }
+
+    /**
+     * Removes the {@link com.group.foctg.holidayMaker.model.Booking} object
+     * with the same <code>id</code> as the parameter from the database.
+     *
+     * @param id Long value used for finding and removing
+     * {@link com.group.foctg.holidayMaker.model.Booking} with that
+     * <code>id</code>
+     * @return A boolean value representing whether the removing was successful
+     * or not
+     */
+    public boolean removeBooking(Long id) {
+        if (bookingRepository.existsById(id)) {
+            bookingRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * If there is a Booking object already that has the same id as the
+     * {@link com.group.foctg.holidayMaker.model.Booking} passed as parameter
+     * then it'll update the existing object. Otherwise it will save the object.
+     *
+     * @param booking {@link com.group.foctg.holidayMaker.model.Booking} object
+     * passed for updating or saving
+     * @return A boolean value representing whether the updating or saving was
+     * successful or not
+     */
+    public boolean updateBooking(Booking booking) {
+        return bookingRepository.saveAndFlush(booking).equals(booking);
+    }
+
+    /**
+     * Goes through the database, checks and returns all
+     * {@link com.group.foctg.holidayMaker.model.Booking} objects in the
+     * List&lt;{@link com.group.foctg.holidayMaker.model.Booking}&gt; if a
+     * customer with given <code>id</code> exists.
+     *
+     * @param id Long value to use for finding the
+     * {@link com.group.foctg.holidayMaker.model.Customer}
+     * @return List&lt;{@link com.group.foctg.holidayMaker.model.Booking}&gt;
+     * from the {@link com.group.foctg.holidayMaker.model.Customer} with the
+     * given <code>id</code>, if it exists
+     */
+    public List<Booking> findBookingsByCustomerId(Long id) {
+        return bookingRepository.findBookingsByCustomerID(id);
+    }
 }
