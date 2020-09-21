d8689aa78e0ef536d4c1bdb68b54d6d59af80d15
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-09-21 21:18:09 +0200

    Removed unused imports and code

--- src/main/java/com/group/foctg/holidayMaker/services/BookingService.java
+++ src/main/java/com/group/foctg/holidayMaker/services/BookingService.java
@@ -21,16 +21,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.group.foctg.holidayMaker.model.Booking;
-import com.group.foctg.holidayMaker.model.DateChecker;
 import com.group.foctg.holidayMaker.model.ReservedDates;
 import com.group.foctg.holidayMaker.model.Room;
 import com.group.foctg.holidayMaker.repositories.BookingRepository;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Service class for the {@link com.group.foctg.holidayMaker.model.Booking}
