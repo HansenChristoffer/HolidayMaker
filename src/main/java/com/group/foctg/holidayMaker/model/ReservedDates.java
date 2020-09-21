7cee998dd2e915f0c8e701ce0a9b0a2bef0e9191
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-09-21 20:02:48 +0200

    ReservedDates entity update
    Added @JsonIdentityInfo class annotation
    Added @JsonIgnoreProperties class annotation
    Added @JsonFormat for the Date types, which forces certain date patterns
    Added @ManyToOne relation with Room and joined column at room_id
    Added @OnetoOne relation with Booking and put orphanRemoval = true, thus booking is removed when ReservedDates is removed
    
    Put the isOverlapping method from DateChecker in ReservedDates

--- src/main/java/com/group/foctg/holidayMaker/model/ReservedDates.java
+++ src/main/java/com/group/foctg/holidayMaker/model/ReservedDates.java
@@ -1,6 +1,10 @@
 package com.group.foctg.holidayMaker.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.CascadeType;
@@ -8,6 +12,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
@@ -17,9 +22,12 @@ import javax.persistence.Temporal;
  * @author Chris
  */
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+//@JsonIdentityReference(alwaysAsId = true)
 public class ReservedDates implements Serializable {
-    
-    public ReservedDates(){
+
+    public ReservedDates() {
     }
 
     public ReservedDates(Date dateFrom, Date dateTo, Room room, Booking booking) {
@@ -28,28 +36,45 @@ public class ReservedDates implements Serializable {
         this.room = room;
         this.booking = booking;
     }
-    
-    @Column
+
+    @Column(name = "reservedDates_id")
     @Id
     @GeneratedValue
     private Long id;
-    
+
     @Column
     @Temporal(javax.persistence.TemporalType.DATE)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private Date dateFrom;
-    
+
     @Column
     @Temporal(javax.persistence.TemporalType.DATE)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private Date dateTo;
-    
-    @ManyToOne
-    @JsonBackReference(value = "rooms_reserveddates")
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "room_id")
+    //@JsonBackReference(value = "rooms_reserveddates")
     private Room room;
-    
-    @OneToOne(cascade = CascadeType.ALL)
-    @JsonBackReference(value = "booking_reserveddates")
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    //@JsonBackReference(value = "booking_reserveddates")
     private Booking booking;
-    
+
+    /**
+     * Method that makes a check if the two ranges overlap or not
+     *
+     * @param dateFrom
+     * @param dateTo
+     * @param dateRangeFrom2
+     * @param dateRangeTo2
+     * @return a boolean value on whether ranges overlap
+     */
+    public boolean isOverlapping(Date dateRangeFrom2, Date dateRangeTo2) {
+        return (dateFrom.getTime() <= dateRangeTo2.getTime()
+                && dateRangeFrom2.getTime() <= dateTo.getTime());
+    }
+
     public Long getId() {
         return id;
     }
@@ -89,7 +114,5 @@ public class ReservedDates implements Serializable {
     public void setBooking(Booking booking) {
         this.booking = booking;
     }
-    
-    
-    
+
 }
